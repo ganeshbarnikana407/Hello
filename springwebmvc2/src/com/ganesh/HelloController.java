6b01e9e1eb16e7e08633a66bf61a1b9352f12ea9
Ganesh <Ganesh@DESKTOP-EVG3S7A>
2019-08-24 22:09:32 +0530

    second

--- springwebmvc2/src/com/ganesh/HelloController.java
+++ springwebmvc2/src/com/ganesh/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	@RequestMapping("/hello")
-	public String redirect()
+			public String redirect()
 	{
 		return "viewpage";
 	}
